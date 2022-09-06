b3e99fd2d33518f7b85a9b1dfb86eb405efa52bd
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2022-09-06 20:45:03 +0500

    I should adapt header for mobile devices

--- js/script.js
+++ js/script.js
@@ -20,3 +20,12 @@ lessThanButton.addEventListener('click', function (e) {
    warning.classList.add('close_');
    topNavbar.classList.remove('paddingTop_');
 });
+
+const element = document.querySelectorAll('.navbar__item[data-da]');
+if (document.documentElement.clientWidth < 767) {
+	element.forEach((el) => {
+		el.removeAttribute('data-da');
+      el.classList.remove('_dynamic_adapt_');
+		console.log(el);
+   });
+}
